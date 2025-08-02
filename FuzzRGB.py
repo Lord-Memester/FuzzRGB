94cf03b2bd5ba8afab375bdcd81b690e6a5cfce0
Lord-Memester <71044282+Lord-Memester@users.noreply.github.com>
2025-08-02 11:49:50 -0500

    Initial Commit

---
+++ FuzzRGB.py
@@ -0,0 +1,19 @@
+# I'm writing this in Python because I assume it's easier to look up libraries for.
+import os, sys
+from PIL import Image, ImageDraw
+
+img = Image.new('RGB', [512, 512],(255,255,255))
+#alright so that's correct but i don't know if it's actually generating an image file. I don't think it is.
+#img.save("test.jpg")
+#hey that works, cool! now I have to figure out how to color specific pixels of the image.
+
+draw = ImageDraw.Draw(img)
+draw.point((0,0), fill=0)
+# i'm gonna try just moving the save statement down here to see if it prevents me from needing to modify the file, since that feels like it would be innefficient
+
+img.save("test2.png")
+#alright that also worked but the picture looks wrong and noisy where that pixel is. Maybe PNG would fix it? | Answer: Yes! very cool. Also, PNG supports transparency, which isn't in use (yet) but could be useful later.
+#I'll probably need a for loop that goes over each pixel of the image and randomizes it. There's probably a FAR better way to make the pixels random but I don't know how yet.
+
+# I don't know if this works and also am not sure how to test it right now. I'm just gonna try stuff.
+# It does not work. Also I think I used the wrong module? More trying stuff time.
