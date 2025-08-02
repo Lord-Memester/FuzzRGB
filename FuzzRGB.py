e08a314ec33c7b01579d2469a8f3c42851e9ad9b
Lord-Memester <71044282+Lord-Memester@users.noreply.github.com>
2025-08-02 15:45:52 -0500

    probably very bad but slightly faster multithreaded version at least for making 512*512 images

--- FuzzRGB.py
+++ FuzzRGB.py
@@ -3,10 +3,17 @@ import os, sys
 from PIL import Image, ImageDraw
 import random
 import math
+import threading
+import concurrent.futures
+import time
+import yappi
 
-width = int(input("Enter image width in pixels: "))
+_NTHREAD = 2
+start_time = time.monotonic_ns()
+
+width = 512 #int(input("Enter image width in pixels: "))
 print(f"Width received: {width}")
-height = int(input("Enter image height in pixels: "))
+height = 512 #int(input("Enter image height in pixels: "))
 print(f"Height received: {height}")
 
 img = Image.new('RGBA', [width, height],(255,255,255, 255))
@@ -14,14 +21,14 @@ img = Image.new('RGBA', [width, height],(255,255,255, 255))
 ycoord = 0
 xcoord = 0
 #numcolors = 3
-#for x in width:
+
 while (xcoord <= width ):
     draw = ImageDraw.Draw(img)
-    rndnum = random.randrange(0,256,255)#(256/numcolors)-1)
-    rndnum2 = random.randrange(0,256,255)#(256/numcolors)-1)
-    rndnum3 = random.randrange(0,256,255)#(256/numcolors)-1)
-    rndnum4 = 255 #random.randrange(0,256,255)#(256/numcolors)-1)#alpha value
-    draw.point((xcoord,ycoord), fill=(rndnum,rndnum2,rndnum3,rndnum4))
+    rndnum1 = random.randrange(0,256,1)
+    rndnum2 = random.randrange(0,256,1)
+    rndnum3 = random.randrange(0,256,1)
+    rndnum4 = 255 #random.randrange(0,256,1) #alpha value
+    draw.point((xcoord,ycoord), fill=(rndnum1,rndnum2,rndnum3,rndnum4))
     #print(rndnum,rndnum2,rndnum3,rndnum4)
     xcoord += 1
     if (xcoord == width):
@@ -30,4 +37,80 @@ while (xcoord <= width ):
     if (ycoord == height):
         break
 
-img.save("test.png") #Epic B)
+#def task1():
+#    subdivision1 = math.floor(width/2)
+#    x1coord = 0
+#    y1coord = 0
+#    while (x1coord <= subdivision1 ):
+#        draw = ImageDraw.Draw(img)
+#        rndnum1 = random.randrange(0,256,1)
+#        rndnum2 = random.randrange(0,256,1)
+#        rndnum3 = random.randrange(0,256,1)
+#        rndnum4 = 255 #random.randrange(0,256,1) #alpha value
+#        draw.point((x1coord,y1coord), fill=(rndnum1,rndnum2,rndnum3,rndnum4))
+#        #print(f"R1: {rndnum1}, G1: {rndnum2}, B1: {rndnum3}, A1: {rndnum4}")
+#        x1coord += 1
+#        if (x1coord == subdivision1):
+#            y1coord += 1
+#            x1coord = 0
+#        if (y1coord == height):
+#            break
+#
+#def task2():
+#    time
+#    subdivision2 = math.ceil(width/2)
+#    x2coord = subdivision2
+#    y2coord = 0
+#    while ((x2coord) <= width):
+#        
+#        draw = ImageDraw.Draw(img)
+#        rndnum1 = random.randrange(0,256,1)
+#        rndnum2 = random.randrange(0,256,1)
+#        rndnum3 = random.randrange(0,256,1)
+#        rndnum4 = 255 #random.randrange(0,256,1) #alpha value
+#        draw.point((x2coord,y2coord), fill=(rndnum1,rndnum2,rndnum3,rndnum4))
+#        #print(f"R2: {rndnum1}, G2: {rndnum2}, B2: {rndnum3}, A2: {rndnum4}")
+#        x2coord += 1
+#        if (x2coord == width):
+#            y2coord += 1
+#            x2coord = subdivision2
+#        if (y2coord == height):
+#            #Time2 = time.process_time()
+#            break
+            
+    #img.save("test.png")
+
+
+# I asked a friend, and they agreed that I could optimize this code with a combination of multithreading and by dividing processing into blocks.
+# I should also implement a profiler to quantatively measure performance.
+
+#blockSize = 4
+
+#if __name__ =="__main__":
+#    t1 = threading.Thread(target=task1)
+#    t2 = threading.Thread(target=task2)
+#
+#    t1.start()
+#    t2.start()
+#
+#    t1.join()
+#    t2.join()
+
+
+end_time = time.monotonic_ns()
+
+print(f"Duration: {end_time - start_time} ns")
+
+
+img.save("test.png")
+
+    #print(f"Thread 2: {Time2}")
+#print("Done!")
+
+
+
+
+
+
+#img1.save("testp1.png") #Epic B)
+#img2.save("testp2.png") #Epic B)
